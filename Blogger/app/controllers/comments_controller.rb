eeca92b085e7608ab35e6da09ba1f4cea92d106f
dionsaputra <dionsaptra666@gmail.com>
2018-06-17 15:05:15 +0700

    Tagging feature completed

--- Blogger/app/controllers/comments_controller.rb
+++ Blogger/app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+    include CommentsHelper
+    
     def new
     end
     
@@ -9,7 +11,4 @@ class CommentsController < ApplicationController
         redirect_to article_path(@comment.article)
     end
 
-    def comment_params
-        params.require(:comment).permit(:author_name, :body)
-    end
 end
